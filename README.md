2c7178bd4da59df204156c83ffd3d703aef6ed59
AdaParra <adapparrad@gmail.com>
2017-11-16 14:20:06 -0400

    Quitando archivos innecesarios, agregando metodos createForm, createInput, createForms a la libreria

--- README.md
+++ README.md
@@ -5,21 +5,51 @@ Prueba práctica para [walmeric](http://www.walmeric.com/).
 
 ## Descripción
 
-Aqui va la descripción
-
-## Instalación 
-
-```
-npm install formlibrary
-```
+Librería que genera un formulario con campos dinámicos
 
 ## Uso
+    
+    * Incluir la libreria en el HTML como un script externo de javascript.
 
-```
-import formlibrary from formlibrary
+    ```
+        <body>
+            ...
+            <script src="./formLibrary.js"></script>
+        </body>
 
-formlibrary(params) //results
-```
+    ```
+
+    ### Creación de un formulario
+
+    1. Utilizar el método createForm disponible en el namespace FormBuilder del objeto window. 
+        ```
+        Ejemplo: 
+        
+        let result = window.FormBuilder.createForm([
+            { type: "text", name: "nombre", id: "idText", required: false},
+            { type: "email", name: "nombre", id: "idNombre", required: false},
+            { type: "email", name: "correo", id: "idCorreo", required: true},
+            { type: "number", name: "telefono", id: "idTelefono", required: false},
+            { type: "text", name: "direccion", id: "idDireccion", required: false}])
+        ```
+    2. El resultado (result) es un objeto con acceso al elemento ``` <form> ``` 
+
+    ### Creación de varios formularios
+
+    1. Utilizar el método createForms disponible en el namespace FormBuilder del objeto window, 
+        con la finalidad de crear varios formularios con sus ids dinamicos incrementales. 
+        ```
+        Ejemplo: 
+
+        let result = window.FormBuilder.createForm([
+            { type: "text", name: "nombre", id: "idText", required: false},
+            { type: "email", name: "nombre", id: "idNombre", required: false},
+            { type: "email", name: "correo", id: "idCorreo", required: true},
+            { type: "number", name: "telefono", id: "idTelefono", required: false},
+            { type: "text", name: "direccion", id: "idDireccion", required: false}], 5)
+        ```
+    2. El resultado (result) es un arreglo de objetos con acceso a los elementos ```<form>```
+
 
 # License 
 [MIT](https://opensource.org/licenses/MIT)
---
+++ formLibrary.js
@@ -0,0 +1,126 @@
+/***
+*   class FormBuilder
+*       Metodos{ 
+*           createForm = Generea un formulario con los campos dinamicos.
+*           createForms = Funcionalidad extra: Genera un formulario con los campos dinamicos
+*                         y los ids dinamicos incrementales.
+*           createInputs = Genera los inputs con los campos dinamicos.
+*       }
+*
+***/
+window.FormBuilder = class FormBuilder{
+
+    /*** 
+    *Inicio createForm
+    *   Recibe: 
+    *       fields: Objeto con los siguientes parametros: 
+    *       {    
+    *           type : "text" // String. Tipos de input: text / number / email... 
+    *           name : "phone" // String. Atributo "name" del campo
+    *           id: "campo_telefono" // String. Id del elemento: #id
+    *           required : true // Bool. SI el campo es requerido: true | false
+    *       }
+    *       callback: funcion defenida por el usuario
+    ***/
+    static createForm(fields, callback){
+
+        let inputs = this.createInputs(fields)
+        let form = document.createElement('form') 
+        
+        inputs.forEach(function(input){
+            form.appendChild(input) 
+        })
+
+        if(callback != undefined) {
+            callback()
+        }
+
+        return form
+    }
+
+    /***
+    * Inicio createInputs
+    ***/
+
+    static createInputs(fields){
+
+        /**
+        * validField: Objetos validados
+        **/
+        const validFields = fields.filter(function(field){
+            
+            /**Si algún objeto posee uno de los parámetros undefine
+            *  ó no es del tipo definido se descarta.  
+            **/
+            if (field.type !== undefined && typeof field.type == 'string' &&
+                field.name !== undefined && typeof field.name == 'string' &&
+                field.id !== undefined && typeof field.id == 'string' &&
+                field.required !== undefined && typeof field.required == 'boolean'){
+                
+                return field
+            }else{
+                console.log("Parámetro(s) incorrecto(s): ", field)
+            }
+        })
+
+        let inputs = validFields.map(function(field, index){ 
+        /**
+        * Por cada objeto de validFields, retorna un <input> 
+        **/
+            let input = document.createElement('input')
+                input.setAttribute('type', field.type)
+                input.setAttribute('name', field.name)
+                input.setAttribute('id', field.id)
+                input.setAttribute('required', field.required)
+
+                return input 
+        
+        })
+        return inputs
+    }
+    /***
+    * Fin createInputs
+    * retorna los objetos inputs con los campos dinamicos  
+    ***/
+
+
+    /***
+    * Inicio createForms
+    * quantity: cantidad de dormularios a generar
+    ***/
+
+    static createForms(fields, quantity, callback){
+
+        let dinamicFields 
+        let forms = []
+
+        for(let i = 0; i < quantity; i++){
+            /***
+            *Generando los ids dinamicos
+            ***/
+            dinamicFields = fields.map(function(field){
+                let newField = Object.assign({}, field)
+                newField.id = newField.id + "-" + i
+
+                return newField
+            })
+
+            forms.push(this.createForm(dinamicFields))
+        }
+
+        if(callback != undefined) {
+            callback()
+        }
+
+        return forms
+
+    }
+    /***
+    * Fin createForms
+    * retorna los objetos form con los campos dinamicos 
+    * y los ids dinamicos incrementales. 
+    ***/
+}
+/***
+* end-FormBuilder
+***/
